9cc8c118ef7a6e12a7aec93f595356c50613955c
Mariya <mariyahorses@yandex.ru>
2023-01-05 18:45:31 +0300

    zadacha 4

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+
+if(N<=1)
+{
+    System.Console.WriteLine("Введите число больше 1");
+    return;
+}
+
+
+    System.Console.WriteLine("Все числа от 1 до " + N);
+    for(int i = 1; i <= N; i++)
+    {
+        System.Console.Write(i + ",");
+    }
